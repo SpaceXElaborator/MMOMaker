a68dc8b5ec3d5d6d81552d744b8ba4574f5c736e
Sean Rahman <terturlminecraft@gmail.com>
2021-11-13 17:51:50 -0800

    V0.73.1 - Comment Updates in DungeonBlock
    To help others understand what I'm trying do, comments have been added to the DungeonBlock.

--- src/main/java/com/terturl/MMO/Dungeon/DungeonBlock.java
+++ src/main/java/com/terturl/MMO/Dungeon/DungeonBlock.java
@@ -33,21 +33,29 @@ public class DungeonBlock {
 	}
 	
 	private void loadBlockMap(Location loc) {
+		// Go through the X Y and Z values
 		for(int x = 0; x < dungeonSchematic.getWidth(); x++) {
 			for(int y = 0; y < dungeonSchematic.getHeight(); y++) {
 				for(int z = 0; z < dungeonSchematic.getLength(); z++) {
+					// Index is taken from the Schematic Website: https://minecraft.fandom.com/wiki/Schematic_file_format
+					// Under the "Blocks" Section
 					int index = y * dungeonSchematic.getWidth() * dungeonSchematic.getLength() + z * dungeonSchematic.getWidth() + x;
+					
+					// Get the block numbers given in a Hex listing using only the buttom 4 bits
 					int b = dungeonSchematic.getBlockIds()[index] & 0xFF;
 					Location blockLoc = new Location(loc.getWorld(), loc.getX() + x, loc.getY() + y, loc.getZ() + z);
 					String s = dungeonSchematic.getPalette().get(b);
 					String mat = "";
 					
+					// Split the string from the JSON format: minecraft:{block}[Information]
+					// This will split and grab just the block so that we can create the Material value
 					if(s.contains("[")) {
 						mat = s.substring(s.indexOf(":")+1, s.indexOf("["));
 					} else {
 						mat = s.substring(s.indexOf(":")+1);
 					}
 					
+					// Create our Schematic Information to store the location, blockdata, and material to be pasted into the world
 					Material m = Material.valueOf(mat.toUpperCase());
 					BlockData bd = Bukkit.getServer().createBlockData(s);
 					dungeonSchematicBlocks.add(new DungeonSchematicBlockInformation(blockLoc, m, bd));
@@ -56,6 +64,9 @@ public class DungeonBlock {
 		}
 	}
 	
+	/**
+	 * Paste the DungeonBlock into the world from its constructor Location
+	 */
 	public void pasteBlock() {
 		for(DungeonSchematicBlockInformation dsbi : dungeonSchematicBlocks) {
 			Bukkit.getConsoleSender().sendMessage(LocationUtils.locationSerializer(dsbi.getDungeonSchematicBlockLocation()));
