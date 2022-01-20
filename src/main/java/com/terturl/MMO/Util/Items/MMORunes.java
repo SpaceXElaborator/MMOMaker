df0879bc04e29ce0afd8511a43b06e811cfc3d24
Sean Rahman <terturlminecraft@gmail.com>
2022-01-20 09:57:37 -0800

    V0.79.0 - Runes * New runes idea to add enhancements to items
    I wanted to add this after looking at this thread -> https://www.spigotmc.org/threads/rpg-like-gem-stone-slots.543223/ Could be a cool way to ensure that people can craft/obtain good weapons and then make them better using these runes

---
+++ src/main/java/com/terturl/MMO/Util/Items/MMORunes.java
@@ -0,0 +1,33 @@
+package com.terturl.MMO.Util.Items;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.inventory.ItemStack;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class MMORunes {
+
+	@Getter
+	private String name;
+	
+	@Getter
+	private ItemStack item;
+	
+	@Getter @Setter
+	private Map<MMOModifiers, Double> modifiers;
+	
+	public MMORunes(String s, ItemStack is) {
+		name = s;
+		item = is;
+		modifiers = new HashMap<MMOModifiers, Double>();
+	}
+	
+	public MMORunes(String s, ItemStack is, Map<MMOModifiers, Double> mods) {
+		this(s, is);
+		modifiers = mods;
+	}
+	
+}
